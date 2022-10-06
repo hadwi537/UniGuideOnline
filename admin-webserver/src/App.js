7d01907fe91f9ce2336cf3632a0624ec7e6b3c20
William of Hadden <williamhadden92@gmail.com>
2022-10-06 13:17:15 +1300

    Added entry info for prereq_list

--- admin-webserver/src/App.js
+++ admin-webserver/src/App.js
@@ -7,6 +7,7 @@ function App() {
       <div className='container' >
       <h1> UniGuide! </h1>
           <p> Enter your paper information </p>
+          <p1> Please enter the items in the Prerequsite list as comma seperated entries</p1>
         </div>
 
         <div className='container'>
