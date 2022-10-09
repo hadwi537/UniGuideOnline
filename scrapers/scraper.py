1dd6671e05568f89e8b7a44c1d6b8ae0c93ce4d6
William of Hadden <williamhadden92@gmail.com>
2022-10-09 15:39:03 +1300

    Created simple lambda_function and wrapped scraper code

--- scrapers/scraper.py
+++ scrapers/scraper.py
@@ -5,107 +5,110 @@ from bs4 import BeautifulSoup
 from connect_to_mongo_db import connect_to_mongo_db
 import re
 
-subject_to_html = dict()
+def scrape():
 
-class OtagoHTMLParser(HTMLParser):
-    def handle_starttag(self, tag, attrs):
-        if (len(attrs) > 1):
-            if (attrs[1][0] == 'title'):
-                #should use the hanmdle data method
-                subject_to_html[attrs[1][1]] = attrs[0][1]
-        
-    def handle_endtag(self, tag):
-        # print("Encountered an end tag :", tag)
-        pass 
+    subject_to_html = dict()
 
-    def handle_data(self, data):
-        # print("Encountered some data  :", data)
-        pass
+    class OtagoHTMLParser(HTMLParser):
+        def handle_starttag(self, tag, attrs):
+            if (len(attrs) > 1):
+                if (attrs[1][0] == 'title'):
+                    #should use the hanmdle data method
+                    subject_to_html[attrs[1][1]] = attrs[0][1]
+            
+        def handle_endtag(self, tag):
+            # print("Encountered an end tag :", tag)
+            pass 
 
-parser = OtagoHTMLParser()
+        def handle_data(self, data):
+            # print("Encountered some data  :", data)
+            pass
 
-A_Z_papers = requests.get('https://www.otago.ac.nz/courses/subjects/a-z/')
-response = A_Z_papers.text
+    parser = OtagoHTMLParser()
 
-parser.feed(response)
+    A_Z_papers = requests.get('https://www.otago.ac.nz/courses/subjects/a-z/')
+    response = A_Z_papers.text
 
-class Paper:
-    '''
-    The addition of a paper descirption would be a good idea
-    '''
+    parser.feed(response)
 
-    def __init__(self, paper_code, year, title, points, teaching_period, subject, prereq_text, prerequistes = "none", more_info='None'):
-        self.paper_code = paper_code
-        self.year = year
-        self.title = title
-        self.points = points
-        self.teaching_period = teaching_period
-        self.subject = subject
-        self.prereq_text = prereq_text
-        self.prerequistes = prerequistes
-        self.more_info = more_info
+    class Paper:
+        '''
+        The addition of a paper descirption would be a good idea
+        '''
 
-    def __repr__(self):
-        return str(self.__dict__)
+        def __init__(self, paper_code, year, title, points, teaching_period, subject, prereq_text, prerequistes = "none", more_info='None'):
+            self.paper_code = paper_code
+            self.year = year
+            self.title = title
+            self.points = points
+            self.teaching_period = teaching_period
+            self.subject = subject
+            self.prereq_text = prereq_text
+            self.prerequistes = prerequistes
+            self.more_info = more_info
 
-
-def get_dl(soup):
-    keys, values = [], []
-    for dl in soup.findAll("dl"):
-        for dt in dl.findAll("dt"):
-            keys.append(dt.text.strip())
-        for dd in dl.findAll("dd"):
-            values.append(dd.text.strip())
-    return dict(zip(keys, values))
+        def __repr__(self):
+            return str(self.__dict__)
 
 
-subject_paperInfo = dict()
-paper_list = list()
-for subject in subject_to_html.keys(): #tABLES ARE options for degree + all papers
-    search = "http://www.otago.ac.nz" + subject_to_html[subject]
-    paperHTML =  requests.get(search)
+    def get_dl(soup):
+        keys, values = [], []
+        for dl in soup.findAll("dl"):
+            for dt in dl.findAll("dt"):
+                keys.append(dt.text.strip())
+            for dd in dl.findAll("dd"):
+                values.append(dd.text.strip())
+        return dict(zip(keys, values))
 
-    papers = None
-    paper_search_defualt = "https://www.otago.ac.nz/courses/papers/index.html?papercode="
-    try:
-        list_of_df = pd.read_html(search)
-        table_num = len(list_of_df) #include if more than n tables 
-        if (table_num > 5): #vauge cutoff point for little v small subjects
-            papers = list_of_df[table_num-1]
-            papers.columns = [c.replace(' ', '_') for c in papers.columns]
-            for row in papers.itertuples():
-                search  = paper_search_defualt + row.Paper_code
-                paperPage = requests.get(search)
-                soup = BeautifulSoup(paperPage.content, "html.parser")
-                dl_dict = get_dl(soup)
 
-                prereq = "none"
-                prereq_list = []
-                try:
-                    prereq = dl_dict["Prerequisite"]
-                    # parse the paper codes
-                    # Search for all capitalised four letter sequences
-                    # Search for all 3 number sequences
-                    pattern =  "[A-Z]{4}[\s]?[0-9]{3}" 
+    subject_paperInfo = dict()
+    paper_list = list()
+    for subject in subject_to_html.keys(): #tABLES ARE options for degree + all papers
+        search = "http://www.otago.ac.nz" + subject_to_html[subject]
+        paperHTML =  requests.get(search)
 
-                    paper_codes = re.findall(pattern, prereq)
+        papers = None
+        paper_search_defualt = "https://www.otago.ac.nz/courses/papers/index.html?papercode="
+        try:
+            list_of_df = pd.read_html(search)
+            table_num = len(list_of_df) #include if more than n tables 
+            if (table_num > 5): #vauge cutoff point for little v small subjects
+                papers = list_of_df[table_num-1]
+                papers.columns = [c.replace(' ', '_') for c in papers.columns]
+                for row in papers.itertuples():
+                    search  = paper_search_defualt + row.Paper_code
+                    paperPage = requests.get(search)
+                    soup = BeautifulSoup(paperPage.content, "html.parser")
+                    dl_dict = get_dl(soup)
+
+                    prereq = "none"
                     prereq_list = []
-                    for paper_code in paper_codes:
-                        cleaned_paper_code = paper_code.replace(" ", "") #remove all whitespace
-                        prereq_list.append(cleaned_paper_code)
-                except:
-                    pass
+                    try:
+                        prereq = dl_dict["Prerequisite"]
+                        # parse the paper codes
+                        # Search for all capitalised four letter sequences
+                        # Search for all 3 number sequences
+                        pattern =  "[A-Z]{4}[\s]?[0-9]{3}" 
 
-                # for mongodb get rid of paper =
-                paper_list.append({'paper_code': row.Paper_code, 'year': row.Year, 'title': row.Title, 'points': row.Points, 'teaching_period': row.Teaching_period, 
-                'subject': subject, 'prereq_string': prereq, 'prereq_list': prereq_list, 'dl_dict': dl_dict})
+                        paper_codes = re.findall(pattern, prereq)
+                        prereq_list = []
+                        for paper_code in paper_codes:
+                            cleaned_paper_code = paper_code.replace(" ", "") #remove all whitespace
+                            prereq_list.append(cleaned_paper_code)
+                    except:
+                        pass
 
-    except Exception as e:
-        print("An exception has occured")
-        print(e)
-    
-    # for testing purposes
-    if (len(paper_list) > 40):
-        break
+                    # for mongodb get rid of paper =
+                    paper_list.append({'paper_code': row.Paper_code, 'year': row.Year, 'title': row.Title, 'points': row.Points, 'teaching_period': row.Teaching_period, 
+                    'subject': subject, 'prereq_string': prereq, 'prereq_list': prereq_list, 'dl_dict': dl_dict})
+
+        except Exception as e:
+            print("An exception has occured")
+            print(e)
+        
+        # for testing purposes
+        if (len(paper_list) > 40):
+            break
+    return paper_list
 
 
