10e94b84606cf72e5fe98396e94e8395c4a3c817
William of Hadden <williamhadden92@gmail.com>
2022-10-09 18:22:18 +1300

    Fixed incorrect dict indexing

--- scrapers/app.py
+++ scrapers/app.py
@@ -8,35 +8,34 @@ def handler(event, context):
     paper_list = scrape()
     print("finished scraping")
     for paper in paper_list:
-        paper = paper[0]
         print(type(paper))
-        print(paper)
+        print("####\n" + str(paper) + "####\n")
+        print(paper['paper_code'])
         print(f'putting ${paper} into table')
         data = client.put_item( 
             TableName='paper_table', 
             Item = {
                 "paper_code": {
-                    'S': paper.paper_code
+                    'S': paper['paper_code']
                 },
                 "year": {
-                    'S': paper.year
+                    'S': str(paper['year'])
                 },
                 "title": {
-                    'S': paper.title
+                    'S': paper['title']
                 },
                 'points': {
-                    'S': paper.points
+                    'N': paper['points']
                 },
                 'subject': {
-                    'S': paper.subject
+                    'S': paper['subject']
                 },
                 'prereq_string': {
-                    'S': paper.prereq_string
+                    'S': paper['prereq_string']
                 },
                 'prereq_list': {
-                    'SS': paper.prereq_list
+                    'SS': paper['prereq_list']
                 }
-            }
-            )
+            })
     
     return "done"
