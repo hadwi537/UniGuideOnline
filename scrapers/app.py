acd48289df6f45b2146e3c9156342da60926d5fb
William of Hadden <williamhadden92@gmail.com>
2022-10-09 17:12:42 +1300

    Added more logging

--- scrapers/app.py
+++ scrapers/app.py
@@ -4,8 +4,11 @@ from scraper import scrape
 client = boto3.client('dynamodb')
 
 def handler(event, context):
+    print("in handler")
     paper_list = scrape()
+    print("finished scraping")
     for paper in paper_list:
+        print(f'putting ${paper} into table')
         data = client.put_item( TableName='paper_table', Item=paper)
     
     return "done"
